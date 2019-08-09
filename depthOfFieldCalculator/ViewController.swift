40a40abecbaf40ec796e3961fd11c573ce8d4fab
wair92 <martin.bednar@yandex.ru>
2019-08-09 16:19:02 +0200

    Initial Commit

---
+++ depthOfFieldCalculator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  depthOfFieldCalculator
+//
+//  Created by Martin Bednar on 09/08/2019.
+//  Copyright © 2019 Martin Bednar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
